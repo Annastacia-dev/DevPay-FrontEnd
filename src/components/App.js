4aada32b2392b0c1a9143c279f6c33f3cc94530b
EliusCaleb <eliuswanyoro@gmail.com>
2022-11-09 15:05:19 +0300

    re-add some file

---
+++ src/components/App.js
@@ -0,0 +1,61 @@
+import React,{useEffect,useState} from 'react'
+import { Route,Routes} from "react-router-dom";
+import Navlink from './Navlink';
+import Home from './Home';
+import About from './About';
+import Service from './Service'; 
+import SignIn from './SignIn';
+import SignUp from './SignUP'
+import '../css/App.css'
+
+
+
+
+
+function App () {
+
+
+  const [currentUser, setCurrentUser] = useState(null)
+
+  const changeUser = (user) => {
+    setCurrentUser(user)
+  }
+
+  // Set developer as user , fetch user from database https://9292/developers
+
+  const [developers, setDevelopers] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:9292/developers')
+      .then(response => response.json())
+      .then(data => setDevelopers(data))
+  }, [])
+
+  return (
+    <div>
+    
+     < Navlink/>
+      <Routes>
+         <Route exact='true' path='/home' element={<Home />} ></Route>
+        <Route exact='true' path='/' element={<Home />} ></Route>
+        <Route exact='true' path='/about' element={<About />} ></Route>
+        <Route path="/signin" element={<SignIn changeUser={changeUser} />} />
+        <Route path="/signup" element={<SignUp />} />      
+        {
+          developers.map(dev => {
+            return <Route key={dev.id} path={`/${dev.id}/service`} element={<Service  />} />
+          }
+          )
+        }
+      </Routes>  
+    
+      
+
+
+
+
+    </div>
+  )
+}
+
+export default App
