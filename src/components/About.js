0e9dc139893bdb98ea2ca0698a18024c90c4c426
Leo <leomutungi19@gmail.com>
2022-11-10 04:31:48 +0300

    changed styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./About.css";
 
 function About() {
   return (
-    <h1>About</h1>
-  )
+    <div className="about">
+      
+      <h2 id="us">About Us!</h2>
+      <h3>Welcome To <span>DevPay</span></h3>
+      <p><span>DevPay</span> is a Professional <span>Payment app</span> Platform. Here we will provide you only, which you will like very much. We're dedicated to providing you the best of <span>Payment app</span>, with a focus on dependability and <span>invoice and making payments</span>. We're working to turn our passion for <span>Payment app</span> into a booming online website. We hope you enjoy our <span>Payment app</span> as much as we enjoy offering them to you.</p>
+      <p>We will keep posting more important posts on my Website for all of you. Please give your support and love.</p>
+      <p>Thanks For Visiting Our Site<br></br>
+      <span>Have a nice day!</span></p>
+    
+    </div>
+  );
 }
 
-export default About
+export default About;
