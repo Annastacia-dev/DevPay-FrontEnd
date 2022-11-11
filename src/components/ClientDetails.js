12ba5b49f89642523745f56a1afb4e58666cec56
EliusCaleb <eliuswanyoro@gmail.com>
2022-11-11 10:25:38 +0300

    clientdetails

--- src/components/ClientDetails.js
+++ src/components/ClientDetails.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
-function ClientDetails({ clientName, clientAddress }) {
+function ClientDetails({ clientName, clientAddress, clientPhone, clientEmail }) {
     return (
         <>
 
             <section className="mt-10">
                 <h2 className="text-2xl uppercase font-bold mb-1">{clientName}</h2>
                 <p>{clientAddress}</p>
+                <p>{clientEmail}</p>
+                <p>{clientPhone}</p>
             </section>
 
         </>
