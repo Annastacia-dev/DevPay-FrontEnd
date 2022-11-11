83d8c4c3b7690cc5d0811b635a9cfb92e019c81e
kelvinclere <kelvinmutethiamuthomi@gmail.com>
2022-11-11 10:42:54 +0300

    developer

--- src/components/Developer.js
+++ src/components/Developer.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-function Developer({ name, address }) {
-   
+function Developer({ name, address, email, phone }) {
     return (
         <>
             <section className='flex  flex-col items-end justify-end'>
-                <h2 className=' font bold text-xl uppercase md:text-4xl'>{name}</h2>
-                <p>{address}</p>
+                <h4 className="text-2xl uppercase font-bold mb-1">{name}</h4>
+                <h5>{address}</h5>
+                <h6>{email}</h6>
+                <h6>{phone}</h6>
             </section>
 
 
