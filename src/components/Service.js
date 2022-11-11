5cb94a6eba740eb215de50bc6d198314640c8d30
Ashley <ashleynjeri7@gmail.com>
2022-11-11 11:14:41 +0300

    service component

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,87 +1,31 @@
 import React from 'react'
+import DashBoardNav from './DashboardNav'
+import '../css/services.css'
 
-function Service() {
-    return (
-
-        <>
-
-            <section className="inner-page">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-8 offset-2">
-                            <form action="" >
-                                <div className="row mt-4">
-                                    {/* <!-- Service input --> */}
-                                    <div className="col form-outline mb-4">
-                                        <input type="text" id="form2Example1" className="form-control" />
-                                        <label className="form-label" >Service name</label>
-                                    </div>
-                                    {/* <!-- service price--> */}
-                                    <div className="col form-outline mb-4">
-                                        <input type="number" id="form2Example1" className="form-control" />
-                                        <label className="form-label" >Service price</label>
-                                    </div>
-                                </div>
-
-                                <div className="row">
-                                    {/* <!-- Service input --> */}
-                                    <div className="col form-outline mb-4">
-
-                                        <select className="form-control" name="cars" id="cars">
-                                            <option value="volvo">Volvo</option>
-                                            <option value="saab">Saab</option>
-                                            <option value="mercedes">Mercedes</option>
-                                            <option value="audi">Audi</option>
-                                        </select>
-                                        <label for="cars">Select Developer:</label>
-
-                                    </div>
-                                </div>
-
-                                    <div className="col form-outline mb-4">
-
-                                        {/* <!-- service price--> */}
-
-                                        <select className="form-control " name="cars" id="cars">
-                                            <option value="volvo">Volvo</option>
-                                            <option value="saab">Saab</option>
-                                            <option value="mercedes">Mercedes</option>
-                                            <option value="audi">Audi</option>
-                                        </select>
-                                        <label for="cars">Select a customer</label>
-
-                                    </div>
-
-
-
-
-                                    {/* <!-- Submit button --> */}
-                                    <button type="submit" className="btn btn-primary btn-md mb-4">Create Invoice</button>
-
-
-                            </form>
-                        </div>
-                    </div>
+const Services = ( { dev}) => {
+  return (
+    <div className='dashboard'>
+        <DashBoardNav />
+        <div className="developer-dashboard services">
+            <h1>Projects History</h1>
+            <div className="services-container">
+                {
+                    dev.services.map(service => {
+                        return (
+                            <div className="service-box" key={service.id}>
+                                <h2>{service.name}</h2>
+                                <p>{service.description}</p>
+                                <p>{service.price}</p>
+                            </div>
+                        )
+                    }
+                    )
+                }
                 </div>
-            </section>
+        </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        </>
-
-    )
+    </div>
+  )
 }
 
-export default Service
+export default Services
