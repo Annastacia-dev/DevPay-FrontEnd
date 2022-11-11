2b3d99950309052cc0334628adcd2a36acfaa187
Ashley <ashleynjeri7@gmail.com>
2022-11-11 11:24:46 +0300

    table component added

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,37 +1,38 @@
 import React from 'react'
 
-function Table({list,total}) {
+function Table({name, description,rate,amount, newService, newClient}) {
   return (
     <>
     <table width="100%" className="mb-10">
-      <thead>
-        <tr className="bg-gray-100 p-1">
-          <td className="font-bold">Description</td>
-          <td className="font-bold">Rate</td>
-          <td className="font-bold">Price</td>
-          <td className="font-bold">Amount</td>
-        </tr>
-      </thead>
-      {list.map(({ id, description, rate, price, amount }) => (
-        <React.Fragment key={id}>
-          <tbody>
-            <tr className="h-10">
-              <td>{description}</td>
-              <td>{rate}</td>
-              <td>{price}</td>
-              <td>{amount}</td>
-            </tr>
-          </tbody>
-        </React.Fragment>
-      ))}
-    </table>
-    <div>
-        <h2 className="flex items-end justify-end text-gray-800 text-4xl font-bold">
-          $ {total}
-        </h2>
-      </div>
+        <thead>
+          <tr className="bg-gray-100 p-1">
+           <td className="font-bold">Name</td>
+            <td className="font-bold">Description</td>
+            <td className="font-bold">Hourly Rate</td>
+            <td className="font-bold">Total Amount</td>
+          </tr>
+        </thead>
+      <tbody>
+      <tr>
+        <td>{name}</td>
+       <td>{description}</td>
+       <td>{rate}</td>
+       <td>{amount}</td>
+      </tr>
 
-  </>
+
+      </tbody>
+
+    </table>
+    
+    
+    
+    
+    
+    
+    
+    
+    </>
   )
 }
 
